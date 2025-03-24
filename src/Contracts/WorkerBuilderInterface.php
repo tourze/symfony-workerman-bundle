61d26939edaf695e309cbac99076654ebeb344d6
tourze-ci <you@example.com>
2025-03-24 03:14:36 +0000

    feat(symfony-workerman-bundle): 添加 Workerman 模块支持

---
+++ src/Contracts/WorkerBuilderInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tourze\Symfony\WorkermanBundle\Contracts;
+
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Workerman\Worker;
+
+#[AutoconfigureTag(WorkerBuilderInterface::WORKER_SERVICE_TAG)]
+interface WorkerBuilderInterface
+{
+    const WORKER_SERVICE_TAG = 'workerman.worker';
+
+    /**
+     * 服务名
+     */
+    public function getName(): string;
+
+    /**
+     * Worker启动时触发
+     *
+     * @see https://manual.workerman.net/doc/zh-cn/worker/on-worker-start.html
+     */
+    public function onWorkerStart(Worker $worker): void;
+
+    /**
+     * Worker停止时触发
+     * 这个在官网文档没介绍
+     */
+    public function onWorkerStop(Worker $worker): void;
+
+    /**
+     * Worker重启
+     *
+     * @see https://manual.workerman.net/doc/zh-cn/worker/on-worker-reload.html
+     */
+    public function onWorkerReload(Worker $worker): void;
+}
---
+++ src/WorkermanBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tourze\Symfony\WorkermanBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WorkermanBundle extends Bundle
+{
+}
